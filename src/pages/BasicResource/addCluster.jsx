3e7f8a7e0aa50d1b37cdc2bc1a5bc603af2357c4
bluemaxs <373807645@qq.com>
2021-09-03 10:02:06 +0800

    feat:集群添加选择组件部署

--- src/pages/BasicResource/addCluster.jsx
+++ src/pages/BasicResource/addCluster.jsx
@@ -8,6 +8,7 @@ import {
 	Grid,
 	Select,
 	Switch,
+	Radio,
 	Message
 } from '@alicloud/console-components';
 import { postCluster, getCluster, putCluster } from '@/services/common.js';
@@ -15,6 +16,7 @@ import pattern from '@/utils/pattern';
 import messageConfig from '@/components/messageConfig';
 import { setRefreshCluster } from '@/redux/globalVar/var';
 
+const { Group: RadioGroup } = Radio;
 const FormItem = Form.Item;
 const formItemLayout = {
 	labelCol: {
@@ -41,6 +43,12 @@ const AddCluster = (props) => {
 	const field = Field.useField();
 	const [dcId, setDcId] = useState('');
 	// const [tcpFlag, setTcpFlag] = useState(true);
+	const [isInstallIngress, setIsInstallIngress] = useState(true);
+	const [isInstallLogging, setIsInstallLogging] = useState(true);
+	const [logCollect, setLogCollect] = useState(true);
+	const [isInstallAlert, setIsInstallAlert] = useState(true);
+	const [isInstallGrafana, setIsInstallGrafana] = useState(true);
+	const [isInstallPrometheus, setIsInstallPrometheus] = useState(true);
 
 	const okHandle = () => {
 		field.validate((err, data) => {
@@ -63,19 +71,6 @@ const AddCluster = (props) => {
 						type: 'harbor',
 						chartRepo: data.chartRepo
 					}
-					// ingress: {
-					// 	address: data.ingressAddress,
-					// 	ingressClassName: data.ingressClassName
-					// },
-					// logging: {
-					// 	elasticSearch: {
-					// 		protocol: data.protocolEs,
-					// 		host: data.hostEs,
-					// 		port: data.portEs,
-					// 		user: data.userEs,
-					// 		password: data.passwordEs
-					// 	}
-					// }
 				};
 				// if (tcpFlag) {
 				// 	sendData.ingress.tcp = {
@@ -88,6 +83,60 @@ const AddCluster = (props) => {
 				// 		enabled: false
 				// 	};
 				// }
+				if (!isInstallIngress) {
+					sendData.ingress = {
+						address: data.ingressAddress,
+						ingressClassName: data.ingressClassName,
+						tcp: {
+							enabled: true,
+							namespace: data.namespace,
+							configMapName: data.configMapName
+						}
+					};
+				}
+				if (!isInstallLogging) {
+					sendData.logging = {
+						elasticSearch: {
+							protocol: data.protocolEs,
+							host: data.hostEs,
+							port: data.portEs,
+							user: data.userEs,
+							password: data.passwordEs,
+							logCollect: data.logCollect
+						}
+					};
+				}
+				if (!isInstallAlert) {
+					sendData.monitor = {
+						...sendData.monitor,
+						alertManager: {
+							host: data.hostAlert,
+							port: data.portAlert,
+							protocol: data.protocolAlert
+						}
+					};
+				}
+				if (!isInstallGrafana) {
+					sendData.monitor = {
+						...sendData.monitor,
+						grafana: {
+							host: data.hostGrafana,
+							port: data.portGrafana,
+							protocol: data.protocolGrafana
+						}
+					};
+				}
+				if (!isInstallPrometheus) {
+					sendData.monitor = {
+						...sendData.monitor,
+						prometheus: {
+							host: data.hostPrometheus,
+							port: data.portPrometheus,
+							protocol: data.protocolPrometheus
+						}
+					};
+				}
+				// console.log(sendData);
 				if (clusterId) {
 					sendData.clusterId = clusterId;
 					sendData.dcId = dcId;
@@ -153,26 +202,50 @@ const AddCluster = (props) => {
 						});
 					}
 					if (res.data.ingress) {
+						setIsInstallIngress(false);
 						field.setValues({
 							ingressAddress: res.data.ingress.address,
-							ingressClassName: res.data.ingress.ingressClassName
+							ingressClassName: res.data.ingress.ingressClassName,
+							namespace: res.data.ingress.tcp.namespace,
+							configMapName: res.data.ingress.tcp.configMapName
 						});
-						if (res.data.ingress.tcp) {
-							field.setValues({
-								tcpFlag: res.data.ingress.tcp.enabled,
-								namespace: res.data.ingress.tcp.namespace,
-								configMapName:
-									res.data.ingress.tcp.configMapName
-							});
-						}
 					}
 					if (res.data.logging && res.data.logging.elasticSearch) {
+						setIsInstallLogging(false);
 						field.setValues({
 							protocolEs: res.data.logging.elasticSearch.protocol,
 							hostEs: res.data.logging.elasticSearch.host,
 							portEs: res.data.logging.elasticSearch.port,
 							userEs: res.data.logging.elasticSearch.user,
-							passwordEs: res.data.logging.elasticSearch.password
+							passwordEs: res.data.logging.elasticSearch.password,
+							logCollect:
+								res.data.logging.elasticSearch.logCollect
+						});
+					}
+					if (res.data.monitor?.alertManager) {
+						setIsInstallAlert(false);
+						field.setValues({
+							protocolAlert:
+								res.data.monitor.alertManager.protocol,
+							hostAlert: res.data.monitor.alertManager.host,
+							portAlert: res.data.monitor.alertManager.port
+						});
+					}
+					if (res.data.monitor?.grafana) {
+						setIsInstallGrafana(false);
+						field.setValues({
+							protocolGrafana: res.data.monitor.grafana.protocol,
+							hostGrafana: res.data.monitor.grafana.host,
+							portGrafana: res.data.monitor.grafana.port
+						});
+					}
+					if (res.data.monitor?.prometheus) {
+						setIsInstallPrometheus(false);
+						field.setValues({
+							protocolPrometheus:
+								res.data.monitor.prometheus.protocol,
+							hostPrometheus: res.data.monitor.prometheus.host,
+							portPrometheus: res.data.monitor.prometheus.port
 						});
 					}
 				}
@@ -181,7 +254,12 @@ const AddCluster = (props) => {
 	}, [clusterId]);
 
 	useEffect(() => {
-		field.setValues({ protocolEs: 'http' });
+		field.setValues({
+			protocolEs: 'http',
+			protocolAlert: 'http',
+			protocolGrafana: 'http',
+			protocolPrometheus: 'http'
+		});
 	}, []);
 
 	return (
@@ -281,72 +359,6 @@ const AddCluster = (props) => {
 						placeholder="请输入AdminConfig"
 					/>
 				</FormItem>
-				{/* <FormItem
-					{...formItemParam}
-					// className=""
-					label="Ingress名称"
-					required
-					requiredMessage="请输入Ingress名称"
-				>
-					<Input
-						htmlType="text"
-						name="ingressClassName"
-						trim={true}
-						defaultValue="nginx-ingress-controller"
-						placeholder="请输入Ingress名称"
-					/>
-				</FormItem>
-				<FormItem
-					{...formItemParam}
-					// className=""
-					label="Ingress地址"
-					required
-					requiredMessage="请输入Ingress地址"
-				>
-					<Input
-						htmlType="text"
-						name="ingressAddress"
-						trim={true}
-						placeholder="请输入主机地址"
-					/>
-				</FormItem> */}
-				{/* <FormItem {...formItemParam} label="Ingress Tcp">
-					<Switch
-						name="tcpFlag"
-						checked={tcpFlag}
-						onChange={(value) => setTcpFlag(value)}
-					/>
-				</FormItem> */}
-				{/* {tcpFlag && (
-					<>
-						<FormItem
-							{...formItemParam}
-							label="ConfigMap分区"
-							required
-							requiredMessage="请输入分区"
-						>
-							<Input
-								htmlType="text"
-								name="namespace"
-								trim={true}
-								placeholder="请输入分区"
-							/>
-						</FormItem>
-						<FormItem
-							{...formItemParam}
-							label="ConfigMap名称"
-							required
-							requiredMessage="请输入ConfigMap名称"
-						>
-							<Input
-								htmlType="text"
-								name="configMapName"
-								trim={true}
-								placeholder="请输入ConfigMap名称"
-							/>
-						</FormItem>
-					</>
-				)} */}
 				<FormItem
 					{...formItemParamNoRequired}
 					label="Harbor地址"
@@ -439,78 +451,352 @@ const AddCluster = (props) => {
 						</Col>
 					</Row>
 				</FormItem>
-				{/* <FormItem
-					{...formItemParam}
-					className=""
-					label="ES地址"
-					style={{ marginBottom: 0 }}
-				>
-					<Row>
-						<Col span={6}>
-							<FormItem>
-								<Select name="protocolEs">
-									<Select.Option value="https">
-										https
-									</Select.Option>
-									<Select.Option value="http">
-										http
-									</Select.Option>
-								</Select>
-							</FormItem>
-						</Col>
-						<Col span={12}>
-							<FormItem
-								pattern={pattern.ip}
-								patternMessage="请输入正确的ip地址！"
-								style={{ marginLeft: -2 }}
-							>
-								<Input
-									htmlType="text"
-									name="hostEs"
-									trim={true}
-									placeholder="请输入主机地址"
-								/>
-							</FormItem>
-						</Col>
-						<Col span={6}>
-							<FormItem style={{ marginLeft: -2 }}>
-								<Input
-									htmlType="number"
-									name="portEs"
-									trim={true}
-									placeholder="端口"
-								/>
-							</FormItem>
-						</Col>
-					</Row>
+				<FormItem {...formItemParam} label="ingress">
+					<RadioGroup
+						dataSource={[
+							{ value: true, label: '安装' },
+							{ value: false, label: '不安装' }
+						]}
+						defaultValue={true}
+						value={isInstallIngress}
+						onChange={(value) => setIsInstallIngress(value)}
+					/>
 				</FormItem>
-				<FormItem
-					{...formItemParam}
-					className=""
-					label="ES鉴权"
-					style={{ marginBottom: 0 }}
-				>
-					<Row gutter={4}>
-						<Col>
-							<FormItem>
-								<Input
-									name="userEs"
-									trim={true}
-									placeholder="请输入用户名"
-								/>
-							</FormItem>
-						</Col>
-						<Col>
-							<FormItem>
-								<Input.Password
-									name="passwordEs"
-									trim={true}
-									placeholder="请输入密码"
-								/>
-							</FormItem>
-						</Col>
-					</Row>
+				{!isInstallIngress && (
+					<>
+						<FormItem
+							{...formItemParam}
+							label="Ingress名称"
+							required
+							requiredMessage="请输入Ingress名称"
+						>
+							<Input
+								htmlType="text"
+								name="ingressClassName"
+								trim={true}
+								defaultValue="nginx-ingress-controller"
+								placeholder="请输入Ingress名称"
+							/>
+						</FormItem>
+						<FormItem
+							{...formItemParam}
+							label="Ingress地址"
+							required
+							requiredMessage="请输入Ingress地址"
+						>
+							<Input
+								htmlType="text"
+								name="ingressAddress"
+								trim={true}
+								placeholder="请输入主机地址"
+							/>
+						</FormItem>
+						<FormItem
+							{...formItemParam}
+							label="ConfigMap分区"
+							required
+							requiredMessage="请输入分区"
+						>
+							<Input
+								htmlType="text"
+								name="namespace"
+								trim={true}
+								placeholder="请输入分区"
+							/>
+						</FormItem>
+						<FormItem
+							{...formItemParam}
+							label="ConfigMap名称"
+							required
+							requiredMessage="请输入ConfigMap名称"
+						>
+							<Input
+								htmlType="text"
+								name="configMapName"
+								trim={true}
+								placeholder="请输入ConfigMap名称"
+							/>
+						</FormItem>
+					</>
+				)}
+				{/* <FormItem {...formItemParam} label="Ingress Tcp">
+					<Switch
+						name="tcpFlag"
+						checked={tcpFlag}
+						onChange={(value) => setTcpFlag(value)}
+					/>
 				</FormItem> */}
+				<FormItem {...formItemParam} label="logging">
+					<RadioGroup
+						dataSource={[
+							{ value: true, label: '安装' },
+							{ value: false, label: '不安装' }
+						]}
+						defaultValue={true}
+						value={isInstallLogging}
+						onChange={(value) => setIsInstallLogging(value)}
+					/>
+				</FormItem>
+				{!isInstallLogging && (
+					<>
+						<FormItem
+							{...formItemParam}
+							label="ES地址"
+							style={{ marginBottom: 0 }}
+						>
+							<Row>
+								<Col span={6}>
+									<FormItem>
+										<Select name="protocolEs">
+											<Select.Option value="https">
+												https
+											</Select.Option>
+											<Select.Option value="http">
+												http
+											</Select.Option>
+										</Select>
+									</FormItem>
+								</Col>
+								<Col span={12}>
+									<FormItem
+										pattern={pattern.ip}
+										patternMessage="请输入正确的ip地址！"
+										style={{ marginLeft: -2 }}
+									>
+										<Input
+											htmlType="text"
+											name="hostEs"
+											trim={true}
+											placeholder="请输入主机地址"
+										/>
+									</FormItem>
+								</Col>
+								<Col span={6}>
+									<FormItem style={{ marginLeft: -2 }}>
+										<Input
+											htmlType="number"
+											name="portEs"
+											trim={true}
+											placeholder="端口"
+										/>
+									</FormItem>
+								</Col>
+							</Row>
+						</FormItem>
+						<FormItem
+							{...formItemParam}
+							// className="formItemParam"
+							label="ES鉴权"
+							style={{ marginBottom: 0 }}
+						>
+							<Row gutter={4}>
+								<Col>
+									<FormItem>
+										<Input
+											name="userEs"
+											trim={true}
+											placeholder="请输入用户名"
+										/>
+									</FormItem>
+								</Col>
+								<Col>
+									<FormItem>
+										<Input.Password
+											name="passwordEs"
+											trim={true}
+											placeholder="请输入密码"
+										/>
+									</FormItem>
+								</Col>
+							</Row>
+						</FormItem>
+						<FormItem
+							{...formItemParam}
+							label="ES日志采集工具"
+							style={{ marginBottom: 0 }}
+						>
+							<Switch
+								name="logCollect"
+								checked={logCollect}
+								onChange={(value) => setLogCollect(value)}
+							/>
+						</FormItem>
+					</>
+				)}
+				<FormItem {...formItemParam} label="监控告警">
+					<RadioGroup
+						dataSource={[
+							{ value: true, label: '安装' },
+							{ value: false, label: '不安装' }
+						]}
+						defaultValue={true}
+						value={isInstallAlert}
+						onChange={(value) => setIsInstallAlert(value)}
+					/>
+				</FormItem>
+				{!isInstallAlert && (
+					<FormItem
+						{...formItemParam}
+						// className="formItemParam"
+						label="监控告警地址"
+						style={{ marginBottom: 0 }}
+					>
+						<Row>
+							<Col span={6}>
+								<FormItem>
+									<Select name="protocolAlert">
+										<Select.Option value="https">
+											https
+										</Select.Option>
+										<Select.Option value="http">
+											http
+										</Select.Option>
+									</Select>
+								</FormItem>
+							</Col>
+							<Col span={12}>
+								<FormItem
+									pattern={pattern.ip}
+									patternMessage="请输入正确的ip地址！"
+									style={{ marginLeft: -2 }}
+								>
+									<Input
+										htmlType="text"
+										name="hostAlert"
+										trim={true}
+										placeholder="请输入主机地址"
+									/>
+								</FormItem>
+							</Col>
+							<Col span={6}>
+								<FormItem style={{ marginLeft: -2 }}>
+									<Input
+										htmlType="number"
+										name="portAlert"
+										trim={true}
+										placeholder="端口"
+									/>
+								</FormItem>
+							</Col>
+						</Row>
+					</FormItem>
+				)}
+				<FormItem {...formItemParam} label="grafana">
+					<RadioGroup
+						dataSource={[
+							{ value: true, label: '安装' },
+							{ value: false, label: '不安装' }
+						]}
+						defaultValue={true}
+						value={isInstallGrafana}
+						onChange={(value) => setIsInstallGrafana(value)}
+					/>
+				</FormItem>
+				{!isInstallGrafana && (
+					<FormItem
+						{...formItemParam}
+						// className="formItemParam"
+						label="grafana地址"
+						style={{ marginBottom: 0 }}
+					>
+						<Row>
+							<Col span={6}>
+								<FormItem>
+									<Select name="protocolGrafana">
+										<Select.Option value="https">
+											https
+										</Select.Option>
+										<Select.Option value="http">
+											http
+										</Select.Option>
+									</Select>
+								</FormItem>
+							</Col>
+							<Col span={12}>
+								<FormItem
+									pattern={pattern.ip}
+									patternMessage="请输入正确的ip地址！"
+									style={{ marginLeft: -2 }}
+								>
+									<Input
+										htmlType="text"
+										name="hostGrafana"
+										trim={true}
+										placeholder="请输入主机地址"
+									/>
+								</FormItem>
+							</Col>
+							<Col span={6}>
+								<FormItem style={{ marginLeft: -2 }}>
+									<Input
+										htmlType="number"
+										name="portGrafana"
+										trim={true}
+										placeholder="端口"
+									/>
+								</FormItem>
+							</Col>
+						</Row>
+					</FormItem>
+				)}
+				<FormItem {...formItemParam} label="prometheus">
+					<RadioGroup
+						dataSource={[
+							{ value: true, label: '安装' },
+							{ value: false, label: '不安装' }
+						]}
+						defaultValue={true}
+						value={isInstallPrometheus}
+						onChange={(value) => setIsInstallPrometheus(value)}
+					/>
+				</FormItem>
+				{!isInstallPrometheus && (
+					<FormItem
+						{...formItemParam}
+						// className="formItemParam"
+						label="prometheus地址"
+						style={{ marginBottom: 0 }}
+					>
+						<Row>
+							<Col span={6}>
+								<FormItem>
+									<Select name="protocolPrometheus">
+										<Select.Option value="https">
+											https
+										</Select.Option>
+										<Select.Option value="http">
+											http
+										</Select.Option>
+									</Select>
+								</FormItem>
+							</Col>
+							<Col span={12}>
+								<FormItem
+									pattern={pattern.ip}
+									patternMessage="请输入正确的ip地址！"
+									style={{ marginLeft: -2 }}
+								>
+									<Input
+										htmlType="text"
+										name="hostPrometheus"
+										trim={true}
+										placeholder="请输入主机地址"
+									/>
+								</FormItem>
+							</Col>
+							<Col span={6}>
+								<FormItem style={{ marginLeft: -2 }}>
+									<Input
+										htmlType="number"
+										name="portPrometheus"
+										trim={true}
+										placeholder="端口"
+									/>
+								</FormItem>
+							</Col>
+						</Row>
+					</FormItem>
+				)}
 			</Form>
 		</Dialog>
 	);
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,9 +1,9 @@
-const apiUrl = 'http://10.1.10.13:31999/api';
-const userUrl = 'http://10.1.10.13:31999/api';
-const wsUrl = 'ws://10.1.10.13:31999/ws';
-// const apiUrl = 'http://10.10.136.6:8080';
-// const userUrl = 'http://10.10.136.6:8080';
-// const wsUrl = 'ws://10.10.136.6:8080/ws';
+// const apiUrl = 'http://10.1.10.13:31999/api';
+// const userUrl = 'http://10.1.10.13:31999/api';
+// const wsUrl = 'ws://10.1.10.13:31999/ws';
+const apiUrl = 'http://10.10.136.12:8080';
+const userUrl = 'http://10.10.136.12:8080';
+const wsUrl = 'ws://10.10.136.12:8080/ws';
 // const apiUrl = 'http://10.10.136.8:8080';
 // const userUrl = 'http://10.10.136.8:8080';
 // const wsUrl = 'ws://10.10.136.8:8080/ws';
