d8599b5ddfaaae9f96f1057f27e1cb06de7f2ea9
bluemaxs <68523341+bluemaxs@users.noreply.github.com>
2021-09-17 13:55:43 +0800

    fix:修复mysql关联实例不存在时的详情跳转问题 (#8)
    * fix:websocket链接兼容wss和ws
    
    * fix:修复备份恢复中克隆实例的跳转传参问题
    
    * fix:添加关联实例已删除的详情跳转判断
    
    Co-authored-by: bluemaxs <373807645@qq.com>

--- src/utils/url.js
+++ src/utils/url.js
@@ -4,12 +4,12 @@
 // const apiUrl = 'http://10.10.136.12:8080';
 // const userUrl = 'http://10.10.136.12:8080';
 // const wsUrl = 'ws://10.10.136.12:8080/ws';
-// const apiUrl = 'http://10.10.136.8:8080';
-// const userUrl = 'http://10.10.136.8:8080';
-// const wsUrl = 'ws://10.10.136.8:8080/ws';
-const apiUrl = 'https://218.75.39.90:31999/api';
-const userUrl = 'https://218.75.39.90:31999/api';
-const wsUrl = 'wss://218.75.39.90:31999/ws';
+const apiUrl = 'http://10.10.136.42:8080';
+const userUrl = '10.10.136.42:8080';
+const wsUrl = 'ws://10.10.136.42:8080/ws';
+// const apiUrl = 'https://218.75.39.90:31999/api';
+// const userUrl = 'https://218.75.39.90:31999/api';
+// const wsUrl = 'wss://218.75.39.90:31999/ws';
 var module;
 module.exports = {
 	apiUrl,
