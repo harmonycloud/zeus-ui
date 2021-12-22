28565763f7f6f7b03d51b0272a018859a2fccdd8
bluemaxs <bluemaxs@aliyun.com>
2021-12-22 15:49:18 +0800

    fix: 灾备中心卡片的跳转;服务列表已删除服务，创建时间显示异常

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,9 +1,9 @@
-const apiUrl = 'http://10.100.100.130:31088/api';
-const userUrl = 'http://10.100.100.130:31088/api';
-const wsUrl = 'ws://10.100.100.130:31088/ws';
-// const apiUrl = 'http://10.10.136.12:8080';
-// const userUrl = 'http://10.10.136.12:8080';
-// const wsUrl = 'ws://10.10.136.12:8080/ws';
+const apiUrl = 'http://10.1.11.126:31088/api';
+const userUrl = 'http://10.1.11.126:31088/api';
+const wsUrl = 'ws://10.1.11.126:31088/ws';
+// const apiUrl = 'http://218.75.39.90:31999/api';
+// const userUrl = 'http://218.75.39.90:31999/api';
+// const wsUrl = 'ws://218.75.39.90:31999/ws';
 // const apiUrl = 'http://10.10.136.31:8080';
 // const userUrl = 'http://10.10.136.31:8080';
 // const wsUrl = 'ws://10.10.136.31:8080/ws';
