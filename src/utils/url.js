58da633a830d620d7401ad3a67c38e7b6444f324
binghui li <1696799004@qq.com>
2022-01-07 16:56:41 +0800

    fix: url更改

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,6 @@
-const apiUrl = 'http://10.1.10.160:31088/api';
-const userUrl = 'http://10.1.10.160:31088/api';
-const wsUrl = 'ws://10.1.10.160:31088/ws';
+// const apiUrl = 'http://10.1.10.160:31088/api';
+// const userUrl = 'http://10.1.10.160:31088/api';
+// const wsUrl = 'ws://10.1.10.160:31088/ws';
 // const apiUrl = 'http://10.10.136.5:8089';
 // const userUrl = 'http://10.10.136.5:8089';
 // const wsUrl = 'ws://10.10.136.5:8089/ws';
