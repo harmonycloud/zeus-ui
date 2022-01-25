16e7f2067634c5a12491dc791d187322a2e3d3f4
bluemaxs <bluemaxs@aliyun.com>
2022-01-25 18:52:34 +0800

    fix: kafka等自定义中间件支持yaml编辑

--- src/pages/InstanceList/Detail/HighAvailability/yamlEdit.tsx
+++ src/pages/InstanceList/Detail/HighAvailability/yamlEdit.tsx
@@ -135,7 +135,7 @@ const YamlEdit = () => {
 							messageConfig('success', '成功', 'yaml编辑成功')
 						);
 						history.push(
-							`/serviceList/${name}/${aliasName}/highAvailability/${middlewareName}/${type}/${chartVersion}`
+							`/serviceList/${name}/${aliasName}/basicInfo/${middlewareName}/${type}/${chartVersion}`
 						);
 					} else {
 						Message.show(messageConfig('error', '失败', res));
