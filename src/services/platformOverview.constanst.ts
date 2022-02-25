04559ebfa24bcc27311bb58a872f54e6b4b7f003
binghui li <1696799004@qq.com>
2022-02-25 15:41:30 +0800

    fix: 告警记录接口更新

--- src/services/platformOverview.constanst.ts
+++ src/services/platformOverview.constanst.ts
@@ -1,5 +1,8 @@
 import { api } from '@/api.json';
 // 平台总览数据
 export const getPlatformOverview = `${api}/platform/overview`;
+// 总览页告警记录
 export const getEvent = `${api}/platform/overview/alertInfo`;
+// 系统和服务告警记录
+export const getEvents = `${api}/platform/overview/alerts`;
 export const getServers = `${api}/platform/overview/middlewareInfo`;
