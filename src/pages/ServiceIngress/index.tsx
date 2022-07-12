659a454dea1fd5968190aa24b99064cdc8c5a712
bluemaxs <bluemaxs@aliyun.com>
2022-07-12 17:35:10 +0800

    feat: 二次菜单服务暴露表格

--- src/pages/ServiceIngress/index.tsx
+++ src/pages/ServiceIngress/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ProHeader, ProPage, ProContent } from '@/components/ProPage';
+import ProTable from '@/components/ProTable';
 
 export default function ServiceIngress(): JSX.Element {
 	return (
@@ -9,7 +10,20 @@ export default function ServiceIngress(): JSX.Element {
 				subTitle="通过Nginx-Ingress/NodePort等多种方式对外暴露已发布的不同类型中间件服务"
 			/>
 			<ProContent>
-				<div></div>
+				<ProTable>
+					<ProTable.Column
+						dataIndex="middlewareName"
+						title="服务名称"
+					/>
+					<ProTable.Column
+						dataIndex="middlewareType"
+						title="服务类型"
+					/>
+					<ProTable.Column dataIndex="exposeType" title="暴露方式" />
+					<ProTable.Column dataIndex="middleware" title="暴露服务" />
+					<ProTable.Column dataIndex="ip" title="暴露IP/域名/端口" />
+					<ProTable.Column dataIndex="action" title="操作" />
+				</ProTable>
 			</ProContent>
 		</ProPage>
 	);
