9b20e6e22fcc39d9294b65899668da5314048b69
binghui li <1696799004@qq.com>
2022-07-21 21:12:57 +0800

    fix: #2780: 服务告警，告警通知设置，优化通知方式

--- src/pages/ServiceListDetail/ServeAlarm/alarmSet.tsx
+++ src/pages/ServiceListDetail/ServeAlarm/alarmSet.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { Checkbox, Form, Modal, Select, notification } from 'antd';
+import { Checkbox, Form, Modal, Select, notification, Tooltip } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 import {
 	getAlarmSetting,
 	getSystemAlarmSetting,
@@ -8,6 +9,7 @@ import {
 	postSystemAlarmSetting
 } from '@/services/alarm';
 import { getUserList } from '@/services/user';
+import { getMailInfo, getDing } from '@/services/alarm';
 
 import { userProps } from '@/pages/UserManage/user';
 
@@ -23,14 +25,28 @@ function AlarmSet(props: any): JSX.Element {
 	} = props;
 	const [form] = Form.useForm();
 	const [users, setUsers] = useState<userProps[]>([]);
+	const [dingDisabled, setDingDisabled] = useState<boolean>(false);
+	const [mailDisabled, setMailDisabled] = useState<boolean>(false);
 	const options = [
-		{ label: '邮箱', value: 'enableMailAlert' },
-		{ label: '钉钉', value: 'enableDingAlert' }
+		{ label: '邮箱', value: 'enableMailAlert', disabled: mailDisabled },
+		{ label: '钉钉', value: 'enableDingAlert', disabled: dingDisabled }
 	];
 
 	useEffect(() => {
 		getUserList({ keyword: '' }).then((res) => {
 			setUsers(res.data);
+		});
+		getMailInfo().then((res) => {
+			if (res.success) {
+				res.data ? setMailDisabled(false) : setMailDisabled(true);
+			}
+		});
+		getDing().then((res) => {
+			if (res.success) {
+				res.data && res.data.length
+					? setDingDisabled(false)
+					: setDingDisabled(true);
+			}
 		});
 	}, []);
 
@@ -128,20 +144,36 @@ function AlarmSet(props: any): JSX.Element {
 			getContainer={false}
 		>
 			<h2>告警通知</h2>
-			<Form labelCol={{ span: 4 }} labelAlign="left" form={form}>
+			<Form labelCol={{ span: 5 }} labelAlign="left" form={form}>
 				<Form.Item
-					label="通知方式"
+					label={
+						<span>
+							<span>通知方式</span>
+							<Tooltip title="在告警设置页面填写对应的告警方式的信息后方可勾选">
+								<QuestionCircleOutlined
+									style={{ marginLeft: 4 }}
+								/>
+							</Tooltip>
+						</span>
+					}
 					name="way"
-					rules={[{ required: true, message: '请选择通知方式' }]}
+					rules={[
+						{
+							required: !mailDisabled && !dingDisabled,
+							message: '请选择通知方式'
+						}
+					]}
 				>
 					<Checkbox.Group options={options} />
 				</Form.Item>
 				<Form.Item
 					label="告警联系人"
 					name="userIds"
-					rules={[{ required: true, message: '请选择告警联系人' }]}
+					rules={[
+						{ required: !mailDisabled, message: '请选择告警联系人' }
+					]}
 				>
-					<Select mode="multiple">
+					<Select mode="multiple" disabled={mailDisabled}>
 						{users.map((item) => {
 							return (
 								<Select.Option key={item.id} value={item.id}>
