6ede93c8458170ca8c07a8a064fe6c615ddeb654
bluemaxs <bluemaxs@aliyun.com>
2022-07-26 17:27:54 +0800

    fix:  mysql备服务隐藏数据库管理tab页

--- src/pages/ServiceListDetail/index.tsx
+++ src/pages/ServiceListDetail/index.tsx
@@ -524,7 +524,21 @@ const InstanceDetails = (props: InstanceDetailsProps) => {
 							{childrenRender('disaster')}
 						</TabPane>
 					) : null}
-					{operateFlag && type === 'mysql' ? (
+					{/* {operateFlag &&
+						type === 'mysql' &&
+						data?.mysqlDTO?.openDisasterRecoveryMode &&
+						data?.mysqlDTO?.isSource === true} */}
+					{operateFlag &&
+					type === 'mysql' &&
+					!data?.mysqlDTO?.openDisasterRecoveryMode ? (
+						<TabPane tab="数据库管理" key="database">
+							{childrenRender('database')}
+						</TabPane>
+					) : null}
+					{operateFlag &&
+					type === 'mysql' &&
+					data?.mysqlDTO?.openDisasterRecoveryMode &&
+					data?.mysqlDTO?.isSource === true ? (
 						<TabPane tab="数据库管理" key="database">
 							{childrenRender('database')}
 						</TabPane>
