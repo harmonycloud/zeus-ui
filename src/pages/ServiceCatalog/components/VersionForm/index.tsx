35802b85fbefea2db900bcf227922ce2aee725e7
bluemaxs <bluemaxs@aliyun.com>
2022-12-12 16:50:26 +0800

    fix: 版本细化，选择大版本后，默认选择第一个小版本

--- src/pages/ServiceCatalog/components/VersionForm/index.tsx
+++ src/pages/ServiceCatalog/components/VersionForm/index.tsx
@@ -69,7 +69,7 @@ export default function VersionForm(props: VersionFormProps): JSX.Element {
 									};
 								})
 							);
-							setVersion('');
+							setVersion(versionOriginData[value][0]);
 						}}
 						disabled={disabled}
 					/>
